cdf17e2bd9c5ed99374b00d54846bb7b07d79af8
Jesus Vega <chuyspc@gmail.com>
2017-04-07 02:57:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function raspyDoorGuy() {
+  const password = 'yarr';
+
+  function givePassword(givenPassword) {
+    if (givenPassword === password) {
+      console.log('Ye may enter.');
+    } else {
+      console.log('Begone, landlubber!');
+    }
+  }
+
+  function willBreakPrinciples(bribeAmount) { // willBreakPrinciples() is the private method
+    return bribeAmount >= 50;
+  }
+
+  function bribe(amount) {
+    if (willBreakPrinciples(amount)) {
+      return password;
+    } else {
+      console.log("Pssht. That won't work.");
+    }
+  }
+
+  return {
+    givePassword,
+    bribe
+  };
+}
